a77eba433547c4af1d4a40c2d579adf855438457
Pitos <contact@gawsoft.com>
2024-02-20 07:29:39 +0100

    Update docs

---
+++ docs/assets/navigation.js
@@ -0,0 +1,1 @@
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA4XRwW7CMAwG4HfxuVqhYjB6rTiiTaMSh4lDyMwarXWi2EVFE++OWKRCOhhX//4/RfHHDwh2Ajm8SoW+PDiEBJySCnJAahtO++CpkqaGBL4NfUL+ckz67kp7ROLKyi0gTu8rRW2Q5NLWtWJGTsM87o2z6+Yat2deORN2F51GJ8bSX+zu6n9+WC0s7czXhZSD618XstjIRvPZ+Dm7chadeKXlHdlZYhxSg/iR9qa8aniIhOmjbmFpj/73IKVdmgbjw+1a0udP4fTmYqxPJ8fNCfsEQMBIAgAA"
